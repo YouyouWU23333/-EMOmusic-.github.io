c715049a6c69b10f931d0de0e302eeccd4410ec2
YouyouWU23333 <148825417+YouyouWU23333@users.noreply.github.com>
2023-10-26 10:42:04 -0500

    Update JS_Mondstadt.js

--- JS_Mondstadt.js
+++ JS_Mondstadt.js
@@ -2,12 +2,12 @@
 
 function Function_1(x) {
     if (x.matches) {
-        document.getElementById('PICrep').style.cssText = "background: url(Image/PICreplace1M.png);";
+        document.getElementById('PICrep').style.cssText = "background: url(PICreplace1M.png);";
         document.getElementById("tp5").innerHTML = "Anemo Archon Statue"
         document.getElementById("tp6").innerHTML = "The Anemo Archon Statue is a point of interest located in Mondstadt City, Starfell Valley, Mondstadt. It is located at the plaza in front of the Favonius Cathedral. The statue is in the image of the Anemo Archon, Barbatos, with a hood, two wings, and its hands cupped together. Therhe is a sentence carved into the statue's pedestal, reading \"The Gateway of Celestia\" in Teyvat language."
 
     } else {
-        document.getElementById('PICrep').style.cssText = "background: url(Image/PICreplace1.png);";
+        document.getElementById('PICrep').style.cssText = "background: url(PICreplace1.png);";
         document.getElementById("tp5").innerHTML = "Anemo Archon Statue"
         document.getElementById("tp6").innerHTML = "The Anemo Archon Statue is a point of interest located in Mondstadt City, Starfell Valley, Mondstadt. It is located at the plaza in front of the Favonius Cathedral. The statue is in the image of the Anemo Archon, Barbatos, with a hood, two wings, and its hands cupped together. Therhe is a sentence carved into the statue's pedestal, reading \"The Gateway of Celestia\" in Teyvat language."
     }
@@ -18,13 +18,13 @@ x.addListener(myFunction) // Attach listener function on state changes
 
 function Function_2(x) {
     if (x.matches) {
-        document.getElementById('PICrep').style.cssText = "background: url(Image/PICreplace2_m.png);";
+        document.getElementById('PICrep').style.cssText = "background: url(PICreplace2_m.png);";
         document.getElementById("tp5").innerHTML = "Knights of Favonius Headquarters"
         document.getElementById("tp6").innerHTML = "Knights of Favonius Headquarters is a point of interest and an instance located in Mondstadt City, Starfell Valley, Mondstadt.\n" +
             "It is the headquarters of the Knights of Favonius. There are two rooms inside that can be entered: the Grand Master's Office and the Mondstadt Library. There is also a locked solitary confinement room, which is almost exclusively used by Klee, as well as an upstairs area that is currently untraversable."
 
     } else {
-        document.getElementById('PICrep').style.cssText = "background: url(Image/PICreplace2.png);";
+        document.getElementById('PICrep').style.cssText = "background: url(PICreplace2.png);";
         document.getElementById("tp5").innerHTML = "Knights of Favonius Headquarters"
         document.getElementById("tp6").innerHTML = "Knights of Favonius Headquarters is a point of interest and an instance located in Mondstadt City, Starfell Valley, Mondstadt.\n" +
             "It is the headquarters of the Knights of Favonius. There are two rooms inside that can be entered: the Grand Master's Office and the Mondstadt Library. There is also a locked solitary confinement room, which is almost exclusively used by Klee, as well as an upstairs area that is currently untraversable."
@@ -36,12 +36,12 @@ x.addListener(myFunction) // Attach listener function on state changes
 
 function Function_3(x) {
     if (x.matches) {
-        document.getElementById('PICrep').style.cssText = "background: url(Image/PICreplace3_m.png);";
+        document.getElementById('PICrep').style.cssText = "background: url(PICreplace3_m.png);";
         document.getElementById("tp5").innerHTML = "Favonius Cathedral"
         document.getElementById("tp6").innerHTML = "The Favonius Cathedral, also referred to as the Mondstadt Cathedral, is a point of interest and an instance located in Mondstadt City, Starfell Valley, Mondstadt. The cathedral is run by the Church of Favonius and is used in worshipping the Anemo Archon Barbatos. At the back of the Cathedral lies a basement in which the Holy Lyre der Himmel is kept guarded. "
 
     } else {
-        document.getElementById('PICrep').style.cssText = "background: url(Image/PICreplace3.png),none-repeat;";
+        document.getElementById('PICrep').style.cssText = "background: url(PICreplace3.png),none-repeat;";
         document.getElementById("tp5").innerHTML = "Favonius Cathedral"
         document.getElementById("tp6").innerHTML = "The Favonius Cathedral, also referred to as the Mondstadt Cathedral, is a point of interest and an instance located in Mondstadt City, Starfell Valley, Mondstadt. The cathedral is run by the Church of Favonius and is used in worshipping the Anemo Archon Barbatos. At the back of the Cathedral lies a basement in which the Holy Lyre der Himmel is kept guarded. "
     }
